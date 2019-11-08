723e8bcba44b9decd356ee59f4d942f34df8f527
Luís Ferreira <luispaulo_26@hotmail.com>
2019-11-08 20:58:59 +0000

    Icons

--- resources/js/components/WelcomeComponent.vue
+++ resources/js/components/WelcomeComponent.vue
@@ -1,9 +1,28 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer" app></v-navigation-drawer>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-contact-mail</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-app-bar app color="indigo" dark>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title>Application</v-toolbar-title>
     </v-app-bar>
 
@@ -14,7 +33,7 @@
             <v-tooltip left>
               <template v-slot:activator="{ on }">
                 <v-btn :href="source" icon large target="_blank" v-on="on">
-                  <v-icon large>mdi-code-tags</v-icon>
+                  <v-icon></v-icon>
                 </v-btn>
               </template>
               <span>Source</span>
@@ -54,4 +73,3 @@ export default {
   })
 };
 </script>
-
